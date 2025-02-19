404ae00f9da5206a3f7cb383275285789858cb56
Morten Munk <mortenmunk97@gmail.com>
2025-02-19 19:56:04 +0100

    hyprlock refactor

--- hosts/default/modules/wm/hyprlock.nix
+++ hosts/default/modules/wm/hyprlock.nix
@@ -1,8 +1,4 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    hyprlock
-  ];
-
+{...}: {
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -10,64 +6,30 @@
         grace = 1;
       };
 
-      #background = [
-      #{
-      #path = "/etc/nixos/wallpapers/lock.jpg";
-      #blur_size = 5;
-      #blur_passes = 1;
-      #noise = 0.0117;
-      #contrast = 1.3000;
-      #brightness = 0.8000;
-      #vibrancy = 0.2100;
-      #vibrancy_darkness = 0.0;
-      #}
-      #];
-
-      #input-field = [
-      #  {
-      #    size = "250, 50";
-      #    outline_thickness = 3;
-      #    dots_size = 0.33;
-      #    dots_spacing = 0.15;
-      #    dots_center = true;
-      #    outer_color = "rgb(139, 213, 202)"; # teal
-      #    inner_color = "rgb(138, 173, 244)"; # blue
-      #    font_color = "rgb(202, 211, 245)"; # text
-      #    placeholder_text = "<i>Password...</i>";
-      #    hide_input = false;
-      #    position = "0, 200";
-      #    halign = "center";
-      #    valign = "bottom";
-      #  }
-      #];
-
       label = [
         {
+          # WEEK
           text = ''cmd[update:18000000] echo "Week $(date +'%V')"'';
-          #color = "rgb(183, 189, 248)"; #lavender
-          #font_size = 24;
-          #font_family = "JetBrains Mono Nerd Font 10";
-          position = "0, -250";
+          font_size = 24;
+          position = "0, -100";
           halign = "center";
           valign = "top";
         }
         {
+          # DAY - MONTH - YEAR
           text = ''cmd[update:18000000] echo "$(date +'%-d %B %Y')"'';
-          #color = "rgb(183, 189, 248)";
-          #font = 24;
-          #font_family = "JetBrains Mono Nerd Font 10";
-          position = "0, -300";
+          font = 38;
+          position = "0, -150";
           halign = "center";
           valign = "top";
         }
         {
+          # TIME
           text = ''cmd[update:1000] echo "$(date +'%H:%M:%S')"'';
-          #color = "rgb(183, 189, 248)"; # lavender
-          #font_size = 94;
-          #font_family = "JetBrains Mono Nerd Font 10";
-          position = "0, 0";
+          font_size = 80;
+          position = "0, 100";
           halign = "center";
-          valign = "center";
+          valign = "bottom";
         }
       ];
     };

c61323bab1147c1edae89445f5262920a691eeed
Morten Munk <mortenmunk97@gmail.com>
2025-03-16 23:15:25 +0100

    update lock + minor fixes

--- hosts/default/modules/text-editor/vscodium.nix
+++ hosts/default/modules/text-editor/vscodium.nix
@@ -1,10 +1,18 @@
 {pkgs, ...}: {
-    home.packages = with pkgs; [
-        vscodium
-    ];
-    
-    programs.vscode = {
-        enable = true;
-        package = pkgs.vscodium;
+  home.packages = with pkgs; [
+    vscodium
+  ];
+
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    profiles.default = {
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
     };
+  };
+
+  stylix.targets.vscode.profileNames = [
+    "default"
+  ];
 }

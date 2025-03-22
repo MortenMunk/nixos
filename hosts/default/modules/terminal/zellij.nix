fbd52e749b4b5e7d59ad224a9939c4ce4aec3bbb
Morten Munk <mortenmunk97@gmail.com>
2025-03-22 14:36:50 +0100

    lock + hide zellij startup tips

--- hosts/default/modules/terminal/zellij.nix
+++ hosts/default/modules/terminal/zellij.nix
@@ -3,6 +3,7 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
+      show_startup_tips = false;
       simplified_ui = true;
       pane_frames = false;
       copy_on_select = true;

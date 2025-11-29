9d38929f25eeecad0a07e03c30d65c295d0283e4
Morten Munk <mortenmunk97@gmail.com>
2025-11-29 15:01:49 +0100

    niri keybind

--- hosts/chevelle/modules/wm/keybinds.nix
+++ hosts/chevelle/modules/wm/keybinds.nix
@@ -1,9 +1,7 @@
-{
-  programs.niri.settings = {
-    binds = {
-      "Super+Q".spawn = ["kitty"];
-      "Super+C".action = "close-window";
-      "Super+R".spawn = ["rofi" "-show" "drun"];
-    };
+{config, ...}: {
+  programs.niri.settings.binds = with config.lib.niri.actions; {
+    "Super+Q".action = spawn "kitty";
+    "Super+C".action = close-window;
+    "Super+R".action = spawn "rofi" "-show" "drun";
   };
 }

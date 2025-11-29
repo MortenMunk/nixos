c4f66b69bc3bc317356020bbf7c422765103f2a3
Morten Munk <mortenmunk97@gmail.com>
2025-11-29 11:48:25 +0100

    stateversion

--- hosts/chevelle/home.nix
+++ hosts/chevelle/home.nix
@@ -2,6 +2,8 @@
   home.username = "morten";
   home.homeDirectory = "/home/morten";
 
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
   nixpkgs.config.allowUnfree = true;
 
   # Let Home Manager install and manage itself.

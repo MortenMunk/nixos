13af7b22075bc2b0553aa60ca3ef1899f5b863e5
Morten Munk <mortenmunk97@gmail.com>
2025-12-08 22:42:00 +0100

    waybar

--- hosts/chevelle/modules/wm/waybar.nix
+++ hosts/chevelle/modules/wm/waybar.nix
@@ -1,4 +1,6 @@
-{...}: {
+{config, ...}: let
+  stylix = config.lib.stylix.colors.withHashtag;
+in {
   programs.waybar = {
     enable = true;
     systemd.enable = true;
@@ -48,5 +50,126 @@
         spacing = 10;
       };
     };
+
+    style = ''
+      * {
+        font-size:15px;
+        font-family: "CodeNewRoman Nerd Font Propo";
+      }
+
+      window#waybar {
+        all:unset;
+      }
+
+      .modules-left {
+        padding: 7px;
+        margin: 10 0 5 10;
+        border-radius: 10px;
+        background: alpha(${stylix.base00},.6);
+        box-shadow: 0px 0px 2px rgba(0,0,0,.6);
+      }
+
+      .modules-center {
+        padding:7px;
+        margin:10 0 5 0;
+        border-radius: 10px;
+        background: alpha(${stylix.base00},.6);
+        box-shadow: 0px 0px 2px rgba(0,0,0,.6);
+      }
+
+      .modules-right {
+        padding: 7px;
+        margin: 10 10 5 0;
+        border-radius: 10px;
+        background: alpha(${stylix.base00},.6);
+        box-shadow: 0px 0px 2px rgba(0,0,0,.6);
+      }
+
+      tooltop {
+        background: ${stylix.base00};
+        color: ${stylix.base07};
+      }
+
+      #clock:hover, #network.hover, #cpu.hover, #temperature.hover {
+        transition: all .3s ease;
+        color:  ${stylix.base09};
+      }
+
+      #clock {
+        padding: 0px 5px;
+        color: ${stylix.base07};
+      }
+
+      #workspaces {
+        padding: 0px 5px;
+      }
+
+      #workspaces button {
+        all:unset;
+        padding: 0px 5px;
+        color: alpha(${stylix.base09},.4);
+        transition: all .2s ease;
+      }
+
+      #workspaces button:hover {
+        color:rgba(0,0,0,0);
+        border: none;
+        text-shadow: 0px 0px 1.5px rgba(0,0,0, .5);
+        transition: all 1s ease;
+      }
+
+      #workspaces button.active {
+        color: ${stylix.base09};
+        border: none;
+        text-shadow: 0px 0px 2px rgba(0,0,0,.5);
+      }
+
+      #workspaces button.empty {
+        color: rgba(0,0,0,0);
+        border: none;
+        text-shadow: 0px 0px 1.5px rgba(0,0,0,.2);
+      }
+
+      #workspaces button.empty:hover {
+        color: rgba(0,0,0,0);
+        border:none;
+        text-shadow: 0px 0px 1.5px rgba(0,0,0,.5);
+        transition: all 1s ease;
+      }
+
+      #workspaces button.empty.active {
+        color: ${stylix.base09};
+        border: none;
+        text-shadow: 0px 0px 2px rgba(0,0,0,.5);
+      }
+
+      #network {
+        padding: 0px 5px;
+        transition: all .3s ease;
+        color: ${stylix.base07};
+      }
+
+      #cpu,#memory,#temperature{
+        padding: 0px 5px;
+        transition: all .3s ease;
+        color: ${stylix.base07};
+      }
+
+      #tray{
+        padding: 0px 5px;
+        transition: all .3s ease;
+
+      }
+      #tray menu * {
+        padding: 0px 5px;
+        transition: all .3s ease;
+      }
+
+      #tray menu separator {
+        padding: 0px 5px;
+        transition: all .3s ease;
+      }
+
+    '';
   };
 }

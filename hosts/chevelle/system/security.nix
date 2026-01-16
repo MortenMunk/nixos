8f2a74e62bd00bf9eefd14cd2e86599d0de20f78
Morten Munk <mortenmunk97@gmail.com>
2026-01-16 18:36:51 +0100

    gnome keyring

--- hosts/chevelle/system/security.nix
+++ hosts/chevelle/system/security.nix
@@ -4,7 +4,10 @@
     sudo-rs.enable = true;
     # Enabled to allow Hyprlock
     # pam.services.hyprlock = {};
+    pam.services.greetd.enableGnomeKeyring = true;
   };
+
+  services.gnome.gnome-keyring.enable = true;
 
   environment.systemPackages = with pkgs; [
     libsecret
